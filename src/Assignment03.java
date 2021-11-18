deb84c279b1fde4108a0e17cc4cc0c16664e607b
Unknown3789 <51219052+Unknown3789@users.noreply.github.com>
2021-11-18 18:50:21 +0800

    Controller Clean-up

--- src/Assignment03.java
+++ src/Assignment03.java
@@ -1,18 +1,12 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Assignment03 {
 
     private MediaBackEnd mediaBackEnd;
 
     public Assignment03() {
         this.mediaBackEnd = new MediaBackEnd();
-
     }
 
     public static void main(String[] args) {
-    Assignment03 startUp = new Assignment03();
+        Assignment03 startUp = new Assignment03();
     }
 }
