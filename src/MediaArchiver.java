746d5e628531a7f0de2724df86f1847330601bf0
Unknown3789 <51219052+Unknown3789@users.noreply.github.com>
2021-11-21 19:41:25 +0800

    Naming Convention Update

--- src/MediaArchiver.java
+++ src/MediaArchiver.java
@@ -1,14 +1,14 @@
-public class Assignment03 {
+public class MediaArchiver {
 
     private MediaBackEnd mediaBackEnd;
     private MediaFrontEnd mediaFrontEnd;
 
-    public Assignment03() {
+    public MediaArchiver() {
         this.mediaBackEnd = new MediaBackEnd();
         this.mediaFrontEnd = new MediaFrontEnd(this.mediaBackEnd);
     }
 
     public static void main(String[] args) {
-        Assignment03 startUp = new Assignment03();
+        MediaArchiver startUp = new MediaArchiver();
     }
 }
